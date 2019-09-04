a175bc790b79c35d1aa4fd011a2d304b2d76b68f
Gabriel Dantas <gabriel.dantas@pmm.am.gov.br>
2019-09-04 09:42:50 -0400

    Aplicando os conceitos de Estado

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import PropTypes from "prop-types";
 class Button extends Component {
   //Props default para quando o button não tiver valores atribuidos
   static defaultProps = {
-    children: "Salvar"
+    children: "-"
   };
   //Props default para quando o button não tiver funções atribuidas
-
   static propTypes = {
     onClick: PropTypes.func.isRequired,
     children: PropTypes.string
@@ -19,37 +18,39 @@ class Button extends Component {
   render() {
     return (
       //passando os valores para o componet gerados no App Component
-      <a onClick={this.props.onClick} href="">
+      <button onClick={this.props.onClick} href="">
         {this.props.children}
-      </a>
+      </button>
     );
-  }
-}
-
-class Input extends Component {
-  render() {
-    return <input placeholder="Pesquise aqui"></input>;
   }
 }
 
 //Classe padrão para a exibição do conteúdo
 class App extends Component {
-  handleClick() {
-    alert("google");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  dishandleClick = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  setZero = () => {
+    this.setState({ counter: 0 });
+  };
   render() {
     return (
       //Para chamar mais de um component dentro de outro, é necessário coloca-los dentro de uma Div ou usar Fragment Component
       <Fragment>
         <h1>Hello React</h1>
-        <Button onClick={this.handleClick}>Doodle</Button>
-        <Button
-          onClick={() => {
-            alert("Salvo!");
-          }}
-        />
+        <Button onClick={this.handleClick}>+</Button>
 
-        <Input />
+        <h2>{this.state.counter}</h2>
+
+        <Button onClick={this.dishandleClick} />
+        <Button onClick={this.setZero}>Zerar</Button>
       </Fragment>
     );
   }
