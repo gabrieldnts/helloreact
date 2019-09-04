b9628eca96bb07e1a8b8ee94fa440d6829ab86d0
Gabriel Dantas <gabriel.dantas@pmm.am.gov.br>
2019-09-04 10:17:13 -0400

    Aplicando conceitos de Stateless Component

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,20 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export default class Button extends Component {
-  //Props default para quando o button não tiver valores atribuidos
-  static defaultProps = {
-    children: "-"
-  };
-  //Props default para quando o button não tiver funções atribuidas
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    children: PropTypes.string
-  };
-  //único método obrigatório de um Component
-  render() {
-    return (
-      //passando os valores para o componet gerados no App Component
-      <button onClick={this.props.onClick} href="">
-        {this.props.children}
-      </button>
-    );
-  }
-}
+// Sintaxe utilizada para Stateless Component
+const Button = props => (
+  <button onClick={props.onClick} href="">
+    {props.children}
+  </button>
+);
+
+Button.defaultProps = {
+  children: "-"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+export default Button;
