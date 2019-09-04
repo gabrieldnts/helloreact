9a6c11b083a1365ae49e635c02a1ec77621b194c
Gabriel Dantas <gabriel.dantas@pmm.am.gov.br>
2019-09-04 08:50:50 -0400

    Aplicando o conceito de propriedades

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
+//Importando biblioteca react e react-dom
 import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 
 class Button extends Component {
-  render() {
-    return <a href="">Google</a>;
-  }
-}
-class App extends Component {
+  //único método obrigatório de um Component
   render() {
     return (
+      <a onClick={this.props.onClick} href="">
+        Google
+      </a>
+    );
+  }
+}
+class Input extends Component {
+  render() {
+    return <input placeholder="Pesquise aqui"></input>;
+  }
+}
+
+//Classe padrão para a exibição do conteúdo
+class App extends Component {
+  handleClick() {
+    alert("google");
+  }
+  render() {
+    return (
+      //Para chamar mais de um component dentro de outro, é necessário coloca-los dentro de uma Div ou usar Fragment Component
       <Fragment>
         <h1>Hello React</h1>
-        <Button />
+        <Button onClick={this.handleClick}></Button>
+        <Input />
       </Fragment>
     );
   }
 }
+//Chamando component que será exibido no HTML e em qual div será exibido
 render(<App />, document.getElementById("app"));
